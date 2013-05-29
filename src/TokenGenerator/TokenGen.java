aacdbde565474288116820ef5d3d652da7d31466
Shiang-Yuan Kao <orange@kaosy.org>
2013-05-29 09:52:21 +0800

    Remove unused methods in TokenGen.java

--- src/TokenGenerator/TokenGen.java
+++ src/TokenGenerator/TokenGen.java
@@ -90,39 +90,4 @@ public class TokenGen {
         buf.append(lo);
         return buf.toString();
     }
-    public static String getHexString(byte[] b) {
-        String result = "";
-        for (int i=0; i < b.length; i++) {
-            result +=
-                Integer.toString( ( b[i] & 0xff ) + 0x100, 16).substring( 1 );
-        }
-        return result;
-    }
-    public static String byteArrayToHexString(byte in[]) {
-
-        byte ch = 0x00;
-        int i = 0;
-
-        if (in == null || in.length <= 0) {
-            return null;
-        }
-
-        String pseudo[] = {"0", "1", "2","3", "4", "5", "6", "7", "8","9", "A", "B", "C", "D", "E","F"};
-        StringBuffer out = new StringBuffer(in.length * 2);
-
-        while (i < in.length) {
-
-            ch = (byte) (in[i] & 0xF0); // Strip off high nibble
-            ch = (byte) (ch >>> 4);     // shift the bits down
-            ch = (byte) (ch & 0x0F);    // must do this is high order bit is on!
-
-            out.append(pseudo[ (int) ch]);  // convert the nibble to a String Character
-            ch = (byte) (in[i] & 0x0F);     // Strip off low nibble
-            out.append(pseudo[ (int) ch]);  // convert the nibble to a String Character
-            i++;
-        }
-
-        String rslt = new String(out);
-        return rslt;
-    }
 }
